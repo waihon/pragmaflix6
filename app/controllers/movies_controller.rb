7a349bb2c72c13652e4efce2a1d904456fb4c4fb
Waihon Yew <yewwaihon@gmail.com>
2019-10-20 19:52:51 +0800

    Flash on movie destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,7 +37,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url
+    redirect_to movies_url, alert: "Movie successfully deleted!"
   end
 
 private
