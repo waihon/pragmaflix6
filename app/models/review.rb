d19077b6926dab8aeaf9cb8482a74ee1de07ed5f
Waihon Yew <yewwaihon@gmail.com>
2019-11-12 23:14:45 +0800

    Display a review's star rating as a series of partially filled in stars

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,4 +6,8 @@ class Review < ApplicationRecord
   validates :name, presence: true
   validates :stars, inclusion: { in: STARS, message: "must be between 1 and 5" }
   validates :comment, length: { minimum: 4 }
+
+  def stars_as_percent
+    (stars / 5.0) * 100.0
+  end
 end
