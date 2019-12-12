fd5b63386c6a958874b6e346fa777480997f0c3c
Waihon Yew <yewwaihon@gmail.com>
2019-12-12 20:38:36 +0800

    Add functionaity to edit reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,6 +17,10 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @review = Review.find(params[:id])
+  end
+
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
