208b7c8b10645774051d30731830af178efa87a4
Waihon Yew <yewwaihon@gmail.com>
2019-12-12 17:56:49 +0800

    Remove duplication in a controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,14 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
   def index
-    @movie = Movie.find_by(id: params[:movie_id])
     @reviews = @movie && @movie.reviews  
   end
 
   def new
-    @movie = Movie.find_by(id: params[:movie_id])
     @review = @movie.reviews.new
   end
 
   def create
-    @movie = Movie.find_by(id: params[:movie_id])
     @review = @movie.reviews.new(review_params)
     if @review.save
       redirect_to movie_reviews_url(@movie), notice: "Thanks for your review!"
@@ -25,4 +23,7 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:name, :stars, :comment)
   end
 
+  def set_movie
+    @movie = Movie.find_by(id: params[:movie_id])
+  end
 end
