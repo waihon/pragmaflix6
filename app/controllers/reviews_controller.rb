44eaaaaea36528c0cee6d81dc552a1b896e8cf4b
Waihon Yew <yewwaihon@gmail.com>
2019-12-12 17:02:52 +0800

    Implement the create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,4 +8,21 @@ class ReviewsController < ApplicationController
     @movie = Movie.find_by(id: params[:movie_id])
     @review = @movie.reviews.new
   end
+
+  def create
+    @movie = Movie.find_by(id: params[:movie_id])
+    @review = @movie.reviews.new(review_params)
+    if @review.save
+      redirect_to movie_reviews_url(@movie), notice: "Thanks for your review!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :stars, :comment)
+  end
+
 end
